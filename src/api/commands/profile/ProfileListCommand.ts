a06ffde9662fc9146f91343d0ff9cbd3a06fc24d
zaSmilingIdiot <30551211+zaSmilingIdiot@users.noreply.github.com>
2023-04-05 18:59:04 +0200

    Updates copyright year notification

--- src/api/commands/profile/ProfileListCommand.ts
+++ src/api/commands/profile/ProfileListCommand.ts
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2022, The Particl Market developers
+// Copyright (c) 2017-2023, The Particl Market developers
 // Distributed under the GPL software license, see the accompanying
 // file COPYING or https://github.com/particl/particl-market/blob/develop/LICENSE
 import * as resources from 'resources';
